866061a4d7cfff6c292a97cea22474abec35da4b
Pedro Saratscheff <pasaratscheff@uc.cl>
2015-10-10 20:07:20 -0300

    correccion destino y destinatario en encomienda

--- chilexplox/src/chilexplox/Encomienda.java
+++ chilexplox/src/chilexplox/Encomienda.java
@@ -2,15 +2,18 @@ package chilexplox;
 
 
 public class Encomienda {
-public String estado;
-public String prioridad;
-public int tamano;
-public int id;
-public Encomienda(String est, String prio)
-{
-estado= est;
-prioridad=prio;
+  public String estado;
+  public String prioridad;
+  public int tamano;
+  public int id;
+  public Sucursal destino;
+  public Cliente destinatario;
 
-}
+  public Encomienda(String est, String prio)
+  {
+  estado= est;
+  prioridad=prio;
+
+  }
 
 }
