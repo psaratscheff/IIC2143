c7c6e67681e7e649cb2fe2dbcd6a58b739a0f71d
Pedro Saratscheff <pasaratscheff@uc.cl>
2015-10-10 20:00:08 -0300

    creacion superclase y main

--- chilexplox/src/chilexplox/main.java
+++ chilexplox/src/chilexplox/main.java
@@ -5,6 +5,9 @@
  */
 package chilexplox;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author peter
@@ -16,6 +19,27 @@ public class main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        //----- CREACIONES-----
+        //-----Empresa
+        Empresa emp = new Empresa();
+        //-----Sucursales
+        Sucursal s1 = new Sucursal("Apoquindo 4333", 1000);
+        Sucursal s2 = new Sucursal("Apoquindo 120", 800);
+        emp.sucursales.add(s1);
+        emp.sucursales.add(s2);
+        //-----Empleados
+        List<Integer> h1 = new ArrayList(); h1.add(8); h1.add(13); h1.add(14); h1.add(17);
+        Empleado e1 = new Empleado("Felix", "Schiegg", "fshiegg", "miclavesecreta", h1);
+        List<Integer> h2 = new ArrayList(); h2.add(10); h2.add(16);
+        Empleado e2 = new Empleado("Pedro", "Saratscheff", "psaratscheff", "mIcl4vESuP3rS3creTa", h2);
+        //-----Camiones
+        Camion c1 = new Camion(20, true);
+        Camion c2 = new Camion(20, true);
+        //-----Cliente
+        
+        
+        
     }
     
 }
