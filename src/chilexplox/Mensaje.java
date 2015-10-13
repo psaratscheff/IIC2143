775920c873667cf48316d55867bb3e5ec04c7f5b
thomprycejones <thompj94@gmail.com>
2015-10-12 23:49:56 -0300

    Agregado readme en pdf

--- src/chilexplox/Mensaje.java
+++ src/chilexplox/Mensaje.java
@@ -5,7 +5,7 @@ public class Mensaje {
   public String contenido;
   public boolean urgente;
   //Relacionados
-  //No se si colocar sucursal a la que se dirige, ya que esta contiene sus mensajes - Pedro
+  
 
   public Mensaje(String contenido, boolean urgente)
   {
