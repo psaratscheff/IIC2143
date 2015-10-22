a18addcd8a9aa7851f169c7a2c6ac010bb8019ca
thomprycejones <thompj94@gmail.com>
2015-10-22 19:22:15 -0300

    Modificado IngresoPedido

---
+++ chilexplox/src/chilexplox/FXMLIngresoPedidoController.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chilexplox;
+
+import chilexplox.classes.Empresa;
+import chilexplox.classes.Sucursal;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * FXML Controller class
+ *
+ * @author Thomas Pryce Jones
+ */
+public class FXMLIngresoPedidoController implements Initializable {
+    @FXML
+    private TextField CNombre;
+    @FXML
+    private TextField CApellido;
+    @FXML
+    private TextField CDireccion;
+    @FXML
+    private TextField CRut;
+    @FXML
+    private Button Pagar;
+    @FXML
+    private Button AgregarEncomienda;
+    @FXML
+    private TextField EAncho;
+    @FXML
+    private ChoiceBox<String> EPrioridad;
+    @FXML
+    private TextField EPeso;
+    @FXML
+    private TextField ELargo;
+    @FXML
+    private ChoiceBox<Sucursal> EDestino;
+    @FXML
+    private TextField EOrigen;
+    @FXML
+    private Label TotalPedido;
+
+    Empresa emp;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        EPrioridad.getItems().add("Normal");
+        EPrioridad.getItems().add("Alta");
+        for (Sucursal s: emp.sucursales) 
+        {
+            EDestino.getItems().add(s);
+        }
+        
+    }    
+
+    @FXML
+    private void btnPagar(MouseEvent event) 
+    {
+        
+    }
+
+    @FXML
+    private void btnAgregarEncomienda(MouseEvent event) 
+    {
+        int tamaño = Integer.parseInt(EPeso.getText())*Integer.parseInt(ELargo.getText())*Integer.parseInt(EAncho.getText());
+        String prioridad = EPrioridad.getValue();
+        Sucursal destino = EDestino.getValue();
+    }
+    
+}
