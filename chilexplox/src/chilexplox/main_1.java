ed19c717ccb429dd44b217de1534801bdef749b2
psaratscheff <pasaratscheff@uc.cl>
2015-10-22 16:24:05 -0300

    old_main fully commented

--- chilexplox/src/chilexplox/main_1.java
+++ chilexplox/src/chilexplox/main_1.java
@@ -3,17 +3,17 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package chilexplox;
+/*package chilexplox;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.List;*/
 
-/**
+/*
  *
  * @author peter
  */
 
-/*
+/* 
 public class main {
 
     /**
