70ef6749b76fd236c73d167c5184fdf201050110
psaratscheff <pasaratscheff@uc.cl>
2015-11-22 16:20:04 -0300

    Rut como user para cliente

--- chilexplox/src/chilexplox/classes/Cliente.java
+++ chilexplox/src/chilexplox/classes/Cliente.java
@@ -7,7 +7,6 @@ public class Cliente {
     // RUT Opcional, la persona puede no querer quedar registrada en el sistema
     // Por eso no se agrega al constructor y debe ser modificado posteriormente
     private String rut;
-    private String usuario;
     private String password;
     private String nombre;
     private String apellido;
@@ -20,11 +19,10 @@ public class Cliente {
         //Inicializo los arrays para poder agregar valores
         this.pedidosRealizados = new ArrayList();
     }
-
-    public Cliente(String nombre, String apellido, String direccion, String user, String pass)
+    public Cliente(String nombre, String apellido, String direccion, String rut, String pass)
     {
         this.nombre = nombre;
-        this.usuario=user;
+        this.rut=rut;
         this.password=pass;
         this.apellido = apellido;
         this.direccion = direccion;
@@ -33,8 +31,6 @@ public class Cliente {
     }
     public String getRut()
     {return rut;}
-    public String getUsuario()
-    {return usuario;}
     public String getPassword()
     {return password;}
     public String getNombre()
