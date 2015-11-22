d8397e24e5d31dc7a56a37cebae9dd32cb9b833e
psaratscheff <pasaratscheff@uc.cl>
2015-11-22 16:08:01 -0300

    fix sincronización y registro de usuarios (Empleados/Clientes/Bosses)

--- chilexplox/src/chilexplox/FXMLRegistraseClienteController.java
+++ chilexplox/src/chilexplox/FXMLRegistraseClienteController.java
@@ -65,80 +65,56 @@ public class FXMLRegistraseClienteController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        Firebase postRef;
-        Firebase newPostRef;
-        postRef = emp.fbRef().child("clientes");
-        postRef.addChildEventListener(new ChildEventListener() {
-            // Retrieve new posts as they are added to the database
-            @Override
-            public void onChildAdded(DataSnapshot snapshot, String previousChildKey) {
-                  //System.out.println(snapshot);
-                  Cliente post = snapshot.getValue(chilexplox.classes.Cliente.class);
-                  clientes.add(post);
-       }
-            @Override
-            public void onChildChanged(DataSnapshot ds, String string) {throw new UnsupportedOperationException("Not supported yet.");}
-            @Override
-            public void onChildRemoved(DataSnapshot ds) {throw new UnsupportedOperationException("Not supported yet.");}
-            @Override
-            public void onChildMoved(DataSnapshot ds, String string) {throw new UnsupportedOperationException("Not supported yet.");}
-            @Override
-            public void onCancelled(FirebaseError fe) {throw new UnsupportedOperationException("Not supported yet."); }
-        });
+        //TODO
     } 
     public void btnRegistrarmeAction(MouseEvent event) throws IOException 
     {
-         for(Cliente c:clientes)
-         {
-                  if(c.getUsuario().equals(CUsername.getText()))
-                  {
-                  MessageBox mb = new MessageBox("El nombre de usuario ya esta ocupado.", MessageBoxType.OK_ONLY);
-                    mb.showAndWait();
-                    if (mb.getMessageBoxResult() == MessageBoxResult.OK)
-                    {
-                        System.out.println("OK");
-                    }
-            
-                
-                  }
-                  else
-                  {
-                  if(CClave.getText().equals(CClave2.getText()))
-                  {
-                      //if(CClave.getText().equals(""))
-                      //{//mensaje de que tiene que tener clave
-                      //}
-                          //else
-                          {//se registra
-                              
-                              Firebase postRef;
-                              Firebase newPostRef;
-                              String nombre= CNombre.getText();
-                              String apellido= CApellido.getText();
-                              String rut= CRut.getText();
-                              String direccion= CDireccion.getText();
-                              String username= CUsername.getText();
-                              String clave= CClave.getText();
-                              Cliente c1 = new Cliente(nombre,apellido,direccion,username,clave);
-                              postRef = emp.fbRef().child("clientes");
-                              newPostRef = postRef.child(c1.getUsuario()); newPostRef.setValue(c1);
-                              ((Node)(event.getSource())).getScene().getWindow().hide();
-                          }
-                  }
-                  else
-                  {
-                      MessageBox mb = new MessageBox("Las claves no concuerdan.", MessageBoxType.OK_ONLY);
-                    mb.showAndWait();
-                    if (mb.getMessageBoxResult() == MessageBoxResult.OK)
-                    {
-                        System.out.println("OK");
-                    }
-                  }
-                  }
-                  //System.out.println("Mensaje:" + post.toString());
-            
-         }
+        String nombre= CNombre.getText();
+        String apellido= CApellido.getText();
+        String direccion= CDireccion.getText();
+        String rut= CRut.getText();
+        String username= CUsername.getText();
+        String clave= CClave.getText();
+        String clave2= CClave2.getText();
+        if (nombre.equals("") || apellido.equals("") || direccion.equals("") || rut.equals("") || username.equals("") || clave.equals("") || clave2.equals("") )
+        {
+                MessageBox mb = new MessageBox("Por favor ingrese todos los campos", MessageBoxType.OK_ONLY);
+                mb.showAndWait();
+                if (mb.getMessageBoxResult() == MessageBoxResult.OK)
+                {
+                    System.out.println("OK");
+                }
+                return; // Evito registrar un usuario con campos en blanco (Fallaría el JSON format!)
+        }
+        for(Cliente c:clientes)
+        {
+            if(c.getUsuario().equals(CUsername.getText()))
+            {
+                MessageBox mb = new MessageBox("El nombre de usuario ya esta ocupado.", MessageBoxType.OK_ONLY);
+                mb.showAndWait();
+                if (mb.getMessageBoxResult() == MessageBoxResult.OK)
+                {
+                    System.out.println("OK");
+                }
+                return; // Evito sobreescribir un usuario ya registrado
+            }
+        }
+        if(CClave.getText().equals(CClave2.getText()))
+        {
+            Firebase postRef;
+            Cliente c1 = new Cliente(nombre,apellido,direccion,username,clave);
+            postRef = emp.fbRef().child("clientes");
+            postRef.child(c1.getUsuario()).setValue(c1);
+            ((Node)(event.getSource())).getScene().getWindow().hide();
+        }
+        else
+        {
+            MessageBox mb = new MessageBox("Las claves no concuerdan.", MessageBoxType.OK_ONLY);
+            mb.showAndWait();
+            if (mb.getMessageBoxResult() == MessageBoxResult.OK)
+            {
+                System.out.println("OK");
+            }
+        }
     }
-    
-    
 }
