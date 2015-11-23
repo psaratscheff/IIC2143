f12415d1568925baa76d1ee76842c99dfb896cde
psaratscheff <pasaratscheff@uc.cl>
2015-11-22 21:30:14 -0300

    mover comentarios main al final

--- chilexplox/src/chilexplox/Main.java
+++ chilexplox/src/chilexplox/Main.java
@@ -42,104 +42,6 @@ public class Main extends Application {
     Firebase newPostRef;
     @Override
     public void start(Stage stage) throws Exception {
-        /*Sucursal s1 = new Sucursal("Valdivia", 1000);
-        Empleado e;
-        e = new Empleado("Thomas", "Pryce Jones", "1", "1", s1.getDireccion());
-        
-        postRef = emp.fbRef().child("empleados");
-        newPostRef = postRef.child(e.getUsername()); newPostRef.setValue(e);*/
-        /*
-        //Creacion de empresa inicial
-        List<Integer> h = new ArrayList(); h.add(8); h.add(13); h.add(14); h.add(17);
-        Sucursal s1 = new Sucursal("Valdivia", 1000);
-        Sucursal s2 = new Sucursal("La Serena",50);
-        Sucursal s3 = new Sucursal("Santiago",150);
-        Sucursal s4 = new Sucursal("Arica",250);
-        
-        
-        Empleado e = new Empleado("Thomas", "Pryce Jones", "1", "1", h, s1.getDireccion());
-        //emp.AddEmpleado(e);
-        
-        Cliente c = new Cliente("Pedro","S","Lejos","2","2");
-        //emp.AddCliente(c);
-        
-        Boss b= new Boss("Berni","Ljubetic","3","3");
-        //emp.getjefes().add(b);
-        
-        Encomienda enc1 = new Encomienda("Normal","Urgente",1,s2.getDireccion(),s3.getDireccion(),"Normal");
-        Encomienda enc2 = new Encomienda("Normal","Urgente",1,s2.getDireccion(),s4.getDireccion(),"Normal");
-        s2.getEncomiendasAlmacenadas().add(enc1);
-        s2.getEncomiendasAlmacenadas().add(enc2);
-        
-        Pedido p1 = new Pedido(null);
-        p1.getEncomiendas().add(enc1);
-        p1.getEncomiendas().add(enc2);
-        
-        Camion c1 = new Camion("Charlie", 10, true,"Normal");
-        Camion c2 = new Camion("CharlieII", 20, true,"Normal");
-        Camion c3 = new Camion("Arnold", 5, true,"Refrigerado");
-        Camion c4 = new Camion("Aleph", 5, true,"Radioactivo");
-        s2.getCamionesEstacionados().add(c1);
-        s2.getCamionesEstacionados().add(c2);
-        s2.getCamionesEstacionados().add(c3);
-        s2.getCamionesEstacionados().add(c4);
-        
-        Mensaje m1 = new Mensaje("AAAAAA", true);
-        s2.getMensajesRecibidos().add(m1);
-        s1.getMensajesRecibidos().add(m1);
-        s4.getMensajesRecibidos().add(m1);
-        s3.getMensajesRecibidos().add(m1);
-        // Cargar información a la base de datos
-        Firebase postRef;
-        Firebase newPostRef;
-        
-       
-        postRef = emp.fbRef().child("encomiendas");
-        newPostRef = postRef.push(); String id1 = newPostRef.getKey(); enc1.setId(id1); newPostRef.setValue(enc1);
-        newPostRef = postRef.push(); String id2 = newPostRef.getKey(); enc2.setId(id2); newPostRef.setValue(enc2);
-        
-        postRef = emp.fbRef().child("sucursales");
-        newPostRef = postRef.child(s1.getDireccion()); newPostRef.setValue(s1);
-        newPostRef = postRef.child(s2.getDireccion()); newPostRef.setValue(s2);
-        newPostRef = postRef.child(s3.getDireccion()); newPostRef.setValue(s3);
-        newPostRef = postRef.child(s4.getDireccion()); newPostRef.setValue(s4);
-        
-        Ingreso i1 = new Ingreso(100, new Date(1448078729000L)); // Timestamp universal en milisegundos, L al final para definir que es un long
-        postRef = emp.fbRef().child("ingresos");
-        newPostRef = postRef.push(); newPostRef.setValue(i1);
-        
-        postRef = emp.fbRef().child("clientes");
-        newPostRef = postRef.child(c.getUsuario()); newPostRef.setValue(c);
-        
-        postRef = emp.fbRef().child("jefes");
-        newPostRef = postRef.child(b.getUsername()); newPostRef.setValue(b);
-        
-        postRef = emp.fbRef().child("empleados");
-        newPostRef = postRef.child(e.getUsername()); newPostRef.setValue(e);
-        
-        postRef = emp.fbRef().child("pedidos");
-        newPostRef = postRef.push(); p1.setId(newPostRef.getKey()); newPostRef.setValue(p1);
-        
-        // Para verificar escrituras válidas (Leíbles por código)
-        postRef = emp.fbRef().child("empleados");
-        postRef.addChildEventListener(new ChildEventListener() {
-            // Retrieve new posts as they are added to the database
-            @Override
-            public void onChildAdded(DataSnapshot snapshot, String previousChildKey) {
-                  //System.out.println(snapshot);
-                  Empleado post = snapshot.getValue(Empleado.class);
-                  //System.out.println("Mensaje:" + post.toString());
-            }
-            @Override
-            public void onChildChanged(DataSnapshot ds, String string) {throw new UnsupportedOperationException("Not supported yet.");}
-            @Override
-            public void onChildRemoved(DataSnapshot ds) {throw new UnsupportedOperationException("Not supported yet.");}
-            @Override
-            public void onChildMoved(DataSnapshot ds, String string) {throw new UnsupportedOperationException("Not supported yet.");}
-            @Override
-            public void onCancelled(FirebaseError fe) {throw new UnsupportedOperationException("Not supported yet."); }
-        });
-        /**/
         // Mantener la lista de encomiendas actualizadas
         postRef = emp.fbRef().child("encomiendas");
         postRef.addChildEventListener(new ChildEventListener() {
@@ -192,7 +94,6 @@ public class Main extends Application {
                 System.out.println("ERROR FB-102:" + fe.getMessage());
             }
         });
-        
         //Mantener los usuarios sincronizados
         postRef = emp.fbRef().child("empleados");
         postRef.addChildEventListener(new ChildEventListener() {
@@ -343,13 +244,109 @@ public class Main extends Application {
         stage.setScene(scene);
         stage.show();
     }
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         launch(args);
     }
-    
-    
 }
+
+        /*Sucursal s1 = new Sucursal("Valdivia", 1000);
+        Empleado e;
+        e = new Empleado("Thomas", "Pryce Jones", "1", "1", s1.getDireccion());
+        
+        postRef = emp.fbRef().child("empleados");
+        newPostRef = postRef.child(e.getUsername()); newPostRef.setValue(e);*/
+        /*
+        //Creacion de empresa inicial
+        List<Integer> h = new ArrayList(); h.add(8); h.add(13); h.add(14); h.add(17);
+        Sucursal s1 = new Sucursal("Valdivia", 1000);
+        Sucursal s2 = new Sucursal("La Serena",50);
+        Sucursal s3 = new Sucursal("Santiago",150);
+        Sucursal s4 = new Sucursal("Arica",250);
+        
+        
+        Empleado e = new Empleado("Thomas", "Pryce Jones", "1", "1", h, s1.getDireccion());
+        //emp.AddEmpleado(e);
+        
+        Cliente c = new Cliente("Pedro","S","Lejos","2","2");
+        //emp.AddCliente(c);
+        
+        Boss b= new Boss("Berni","Ljubetic","3","3");
+        //emp.getjefes().add(b);
+        
+        Encomienda enc1 = new Encomienda("Normal","Urgente",1,s2.getDireccion(),s3.getDireccion(),"Normal");
+        Encomienda enc2 = new Encomienda("Normal","Urgente",1,s2.getDireccion(),s4.getDireccion(),"Normal");
+        s2.getEncomiendasAlmacenadas().add(enc1);
+        s2.getEncomiendasAlmacenadas().add(enc2);
+        
+        Pedido p1 = new Pedido(null);
+        p1.getEncomiendas().add(enc1);
+        p1.getEncomiendas().add(enc2);
+        
+        Camion c1 = new Camion("Charlie", 10, true,"Normal");
+        Camion c2 = new Camion("CharlieII", 20, true,"Normal");
+        Camion c3 = new Camion("Arnold", 5, true,"Refrigerado");
+        Camion c4 = new Camion("Aleph", 5, true,"Radioactivo");
+        s2.getCamionesEstacionados().add(c1);
+        s2.getCamionesEstacionados().add(c2);
+        s2.getCamionesEstacionados().add(c3);
+        s2.getCamionesEstacionados().add(c4);
+        
+        Mensaje m1 = new Mensaje("AAAAAA", true);
+        s2.getMensajesRecibidos().add(m1);
+        s1.getMensajesRecibidos().add(m1);
+        s4.getMensajesRecibidos().add(m1);
+        s3.getMensajesRecibidos().add(m1);
+        // Cargar información a la base de datos
+        Firebase postRef;
+        Firebase newPostRef;
+        
+       
+        postRef = emp.fbRef().child("encomiendas");
+        newPostRef = postRef.push(); String id1 = newPostRef.getKey(); enc1.setId(id1); newPostRef.setValue(enc1);
+        newPostRef = postRef.push(); String id2 = newPostRef.getKey(); enc2.setId(id2); newPostRef.setValue(enc2);
+        
+        postRef = emp.fbRef().child("sucursales");
+        newPostRef = postRef.child(s1.getDireccion()); newPostRef.setValue(s1);
+        newPostRef = postRef.child(s2.getDireccion()); newPostRef.setValue(s2);
+        newPostRef = postRef.child(s3.getDireccion()); newPostRef.setValue(s3);
+        newPostRef = postRef.child(s4.getDireccion()); newPostRef.setValue(s4);
+        
+        Ingreso i1 = new Ingreso(100, new Date(1448078729000L)); // Timestamp universal en milisegundos, L al final para definir que es un long
+        postRef = emp.fbRef().child("ingresos");
+        newPostRef = postRef.push(); newPostRef.setValue(i1);
+        
+        postRef = emp.fbRef().child("clientes");
+        newPostRef = postRef.child(c.getUsuario()); newPostRef.setValue(c);
+        
+        postRef = emp.fbRef().child("jefes");
+        newPostRef = postRef.child(b.getUsername()); newPostRef.setValue(b);
+        
+        postRef = emp.fbRef().child("empleados");
+        newPostRef = postRef.child(e.getUsername()); newPostRef.setValue(e);
+        
+        postRef = emp.fbRef().child("pedidos");
+        newPostRef = postRef.push(); p1.setId(newPostRef.getKey()); newPostRef.setValue(p1);
+        
+        // Para verificar escrituras válidas (Leíbles por código)
+        postRef = emp.fbRef().child("empleados");
+        postRef.addChildEventListener(new ChildEventListener() {
+            // Retrieve new posts as they are added to the database
+            @Override
+            public void onChildAdded(DataSnapshot snapshot, String previousChildKey) {
+                  //System.out.println(snapshot);
+                  Empleado post = snapshot.getValue(Empleado.class);
+                  //System.out.println("Mensaje:" + post.toString());
+            }
+            @Override
+            public void onChildChanged(DataSnapshot ds, String string) {throw new UnsupportedOperationException("Not supported yet.");}
+            @Override
+            public void onChildRemoved(DataSnapshot ds) {throw new UnsupportedOperationException("Not supported yet.");}
+            @Override
+            public void onChildMoved(DataSnapshot ds, String string) {throw new UnsupportedOperationException("Not supported yet.");}
+            @Override
+            public void onCancelled(FirebaseError fe) {throw new UnsupportedOperationException("Not supported yet."); }
+        });
+        /**/
